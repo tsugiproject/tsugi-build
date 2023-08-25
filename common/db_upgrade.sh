966f23902a2a8c01e766c717ff71b6690f44a45b
Charles Severance <csev@umich.edu>
2023-08-25 19:06:17 -0400

    Updates

--- common/db_upgrade.sh
+++ common/db_upgrade.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-cd /var/www/html/tsugi/admin
-php upgrade.php
+for f in /var/www/html /var/www/sites/*
+do
+    echo Database upgrade $f
+    cd $f/tsugi
+    if [ -d .git ] ; then
+       cd admin
+       php upgrade.php
+    fi
+done
+
